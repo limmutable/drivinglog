190f7fcec86056a50dd41a5e0a0769fbf94196d1
Jeffrey Lim <limjeong@gmail.com>
2022-07-17 16:29:55 +0900

    now it generates entries of given number of days with some extra random distances

--- main.py
+++ main.py
@@ -2,51 +2,58 @@
 import sys
 import random
 from datetime import datetime
+from datetime import timedelta
 import csv
 
-def getToday():
-    return datetime.today().strftime('%Y-%m-%d')
-
 def generateRandom():
-    print ('Running', sys.argv[0], "...")
-    print ('Generating', sys.argv[1], 'random number(s) ...')
-    # Command line args are in sys.argv[1], sys.argv[2] ...
-    # sys.argv[0] is the script name itself and can be ignored
-    i=0
+        a = random.random()
+        # generate a random integer between 0 - 3
+        #print(a)
+        #print(int(a*4))
+        return (int(a*4))
 
-    repeat = sys.argv[1]
-    for i in range(int(repeat)):
-        a=random.random()
-        print(a)
-        i = i + 1
 
 # Generating a driving record file (csv format)
 # Date (YYYY-MM-DD), From, To, Distance
 # Example of a raw: "2022-07-24, 집, 회사, 24"
 
 def generateReport():
-    print("Generating a report...")
-    
-    header = ['Date', 'From', 'To', 'Distance']
-    data1 = [getToday(), 'Home', 'Work', '24']
-    data2 = [getToday(), 'Work', 'Home', '22']
+    # initialization and user inputs
+    # Enter start date here and how many days to run
+    startdate = datetime(2022, 1, 1)
+    howmanydays = 10
 
+    # console output
+    print("Generating a report from " + startdate.strftime('%Y-%m-%d') + " for " + str(howmanydays) + " days")
+    
+ 
     with open('output.csv', 'w', encoding='UTF8') as f:
         writer = csv.writer(f)
 
+        # csv header row
+        header = ['Date', 'From', 'To', 'Distance']
         # write the header
         writer.writerow(header)
 
-        # write the data from Home to Work
-        writer.writerow(data1)
+        day = 0
+        d = startdate
 
-        # write the data from Work to Home
-        writer.writerow(data2)
+        for day in range(howmanydays):
+            # print("writing " + d.strftime('%Y-%m-%d'))
+
+            # base work-home route = 24km, then add some extra random distance
+            entry1 = [d.strftime('%Y-%m-%d'), 'Home', 'Work', str(24+generateRandom())]
+            entry2 = [d.strftime('%Y-%m-%d'), 'Work', 'Home', str(24+generateRandom())]
+            # write the data from Home to Work
+            writer.writerow(entry1)
+            # write the data from Work to Home
+            writer.writerow(entry2)
+
+            d = d + timedelta(days = 1)
+            day = day + 1
 
 # Gather our code in a main() function
 def main():
-    getToday()
-#    generateRandom()
     generateReport()
 
 
